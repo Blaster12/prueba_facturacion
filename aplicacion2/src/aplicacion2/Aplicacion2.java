43e71538a2d3cd5045a38b74591148842478d977
mariosavala <mariosavala25@gmail.com>
2017-11-19 14:57:22 -0500

    Primera Version, cambios 2 puntitos

--- aplicacion2/src/aplicacion2/Aplicacion2.java
+++ aplicacion2/src/aplicacion2/Aplicacion2.java
@@ -17,7 +17,7 @@ public class Aplicacion2 {
     public static void main(String[] args) {
      
         // TODO code application logic here
-        System.out.println("Hola mundo desde Git");
+        System.out.println("Hola mundo desde Git  ..");
     }
     
 }
