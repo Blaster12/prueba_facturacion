3b2c5de9e83b04ce814c62eb84b661efb650362b
mariosavala <mariosavala25@gmail.com>
2017-11-19 14:49:12 -0500

    Primera Version, cambios 1

--- aplicacion2/src/aplicacion2/Aplicacion2.java
+++ aplicacion2/src/aplicacion2/Aplicacion2.java
@@ -15,7 +15,9 @@ public class Aplicacion2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+     
         // TODO code application logic here
+        System.out.println("Hola mundo desde Git");
     }
     
 }
